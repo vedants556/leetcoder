99d6278561a0ec3ff41af657a1fe4827aeee6627
Vedant Shelar <118654021+vedants556@users.noreply.github.com>
2025-03-21 11:21:25 +0530

    Time: 143 ms (95.00%) | Memory: 21.8 MB (20.63%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,19 +1,15 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        flip_queue = deque()  # Stores indices of flip operations
-        count = 0  # Number of operations performed
-
-        for i in range(len(nums)):
-            # Remove expired flips (older than 3 indices)
-            while flip_queue and i > flip_queue[0] + 2:
-                flip_queue.popleft()
-
-            # If the current element needs flipping
-            if (nums[i] + len(flip_queue)) % 2 == 0:
-                # Cannot flip a full triplet
-                if i + 2 >= len(nums):
-                    return -1
-                count += 1
-                flip_queue.append(i)
-
-        return count
+        def flip(nums, i):
+            nums[i] = 0 if nums[i] else 1
+        
+        res = 0
+        for i in range(len(nums) - 2):
+            if nums[i] == 0:
+                flip(nums, i)
+                flip(nums , i+1)
+                flip(nums, i+2)
+                res += 1
+        if not nums[-1] or not nums[-2]:
+            return -1
+        return res
