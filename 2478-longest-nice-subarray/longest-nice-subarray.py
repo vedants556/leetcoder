1511fcbd359db3f47dddbb8c8bb98fd33a93285d
Vedant Shelar <118654021+vedants556@users.noreply.github.com>
2025-03-21 20:26:39 +0530

    Time: 89 ms (68.33%) | Memory: 31.9 MB (59.60%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -1,24 +1,14 @@
 class Solution:
-    def longestNiceSubarray(self, nums: list[int]) -> int:
-        max_length = 1  # Track the maximum nice subarray length found
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        res = 0 
+        cur = 0
+        l = 0
 
-        for start in range(len(nums) - max_length):
-            current_length = 1  # Length of current nice subarray
-            used_bits = nums[start]  # Track which bits are used in our subarray
+        for r in range(len(nums)):
+            while cur & nums[r]:
+                cur = cur ^ nums[l]
+                l += 1
+            res = max(res, r - l + 1)
+            cur = cur | nums[r]
 
-            # Try to extend the subarray
-            for end in range(start + 1, len(nums)):
-                # If no bits overlap between current number and used bits, we can extend
-                if (used_bits & nums[end]) == 0:
-                    used_bits |= nums[
-                        end
-                    ]  # Add new number's bits to our tracker
-                    current_length += 1
-                # If bits overlap, this number can't be part of our nice subarray
-                else:
-                    break
-
-            # Update max length if we found a longer nice subarray
-            max_length = max(max_length, current_length)
-
-        return max_length
+        return res
