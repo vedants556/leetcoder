eabf8b13bbc7c7d7e9e0dea6bd3dc96ce9f95d4d
Vedant Shelar <118654021+vedants556@users.noreply.github.com>
2025-03-27 17:50:04 +0530

    Time: 111 ms (30.69%) | Memory: 34.8 MB (13.86%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        left = defaultdict(int)
+        right = Counter(nums)
+
+        for i in range(len(nums)):
+            left[nums[i]] += 1
+            right[nums[i]] -= 1
+
+            left_len = i + 1
+            right_len = len(nums) - i - 1
+
+            if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
+                return i
+            
+        return -1
