cbe218b00151c970349e5948d88b81b1650d48d3
Vedant Shelar <118654021+vedants556@users.noreply.github.com>
2025-03-27 18:26:45 +0530

    Time: 267 ms (18.42%) | Memory: 39.7 MB (16.67%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,46 +1,22 @@
 class Solution:
-    def minOperations(self, grid: list[list[int]], x: int) -> int:
-        nums_array = []
-        result = 0
-
-        # Flatten the grid into nums_array and check remainder condition
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                # If any element has a different remainder than the first element,
-                # it is impossible to make all elements equal, so
-                # return -1
-                if grid[row][col] % x != grid[0][0] % x:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        
+        for row in grid:
+            for n in row:
+                if n % x != grid[0][0] % x:
                     return -1
-                nums_array.append(grid[row][col])
+                
+        nums = sorted([n for row in grid for n in row])
 
-        nums_array.sort()
+        prefix = 0
+        total = sum(nums)
+        res = float("inf")
 
-        length = len(nums_array)
-        prefix_index = 0
-        suffix_index = length - 1
+        for i in range(len(nums)):
+            cost_left = nums[i] * i - prefix
+            cost_right = total - prefix - (nums[i] * (len(nums) - i))
+            operations = (cost_left + cost_right) // x
+            res = min(res , operations)
+            prefix += nums[i]
 
-        # Move prefix_index and suffix_index towards the middle
-        while prefix_index < suffix_index:
-            # If the prefix of equal elements is shorter than the suffix
-            if prefix_index < length - suffix_index - 1:
-                # Calculate the number of operations required to extend the prefix
-                prefix_operations = (
-                    (prefix_index + 1)
-                    * (nums_array[prefix_index + 1] - nums_array[prefix_index])
-                    // x
-                )
-                result += prefix_operations
-                # Move the prefix index forward
-                prefix_index += 1
-            else:
-                # Calculate the number of operations required to extend the suffix
-                suffix_operations = (
-                    (length - suffix_index)
-                    * (nums_array[suffix_index] - nums_array[suffix_index - 1])
-                    // x
-                )
-                result += suffix_operations
-                # Move the suffix index backward
-                suffix_index -= 1
-
-        return result
+        return res
